6984f5d5d75049eb79d59c64f6dae7c87962c1e6
Alexandre Ouicher <aouicher@evene.fr>
2013-05-31 11:13:16 +0200

    mise en place des taches de pilotage

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/auto', routes.index);
 app.get('/users', user.list);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
